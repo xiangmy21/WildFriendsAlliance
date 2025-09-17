532661ff2fc1a04299426fef1fd0a2012222624d
Wukai <fisterist@gmail.com>
2025-09-17 18:21:11 +0800

    修改问答的问题 UI

--- Assets/Scripts/QuestionUI.cs
+++ Assets/Scripts/QuestionUI.cs
@@ -62,19 +62,59 @@ public class QuestionUI : MonoBehaviour
             questionText.text = cardData.question.question;
         }
 
-        // 设置选项文本
+        // 加载按钮背景图片
+        Sprite buttonSprite = null;
+#if UNITY_EDITOR
+        buttonSprite = UnityEditor.AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Images/UI/button1.png");
+#else
+        buttonSprite = Resources.Load<Sprite>("Images/UI/button1");
+#endif
+
+        // 设置选项文本和按钮样式
         for (int i = 0; i < optionTexts.Length && i < cardData.question.options.Length; i++)
         {
             if (optionTexts[i])
             {
                 optionTexts[i].text = cardData.question.options[i];
+                optionTexts[i].color = Color.black; // 设置文字颜色为黑色
             }
 
-            // 激活对应的按钮
+            // 设置按钮背景和尺寸
             if (optionButtons[i])
             {
                 optionButtons[i].gameObject.SetActive(true);
                 optionButtons[i].interactable = true;
+
+                // 设置按钮背景图片
+                Image buttonImage = optionButtons[i].GetComponent<Image>();
+                if (buttonImage && buttonSprite)
+                {
+                    buttonImage.sprite = buttonSprite;
+                    buttonImage.type = Image.Type.Sliced; // 使用切片模式
+                }
+
+                // 设置按钮尺寸
+                RectTransform buttonRect = optionButtons[i].GetComponent<RectTransform>();
+                if (buttonRect)
+                {
+                    // 禁用可能的布局组件
+                    LayoutElement layoutElement = optionButtons[i].GetComponent<LayoutElement>();
+                    if (layoutElement)
+                    {
+                        layoutElement.ignoreLayout = true;
+                    }
+
+                    // 强制设置尺寸
+                    buttonRect.sizeDelta = new Vector2(180f, 42f); // 宽180（-10%），高42（+20%）
+
+                    // 强制刷新布局
+                    LayoutRebuilder.ForceRebuildLayoutImmediate(buttonRect);
+
+                    // 设置按钮间距 - 紧凑排列，间隔5像素
+                    Vector2 pos = buttonRect.anchoredPosition;
+                    pos.y = 50f - (i * 47f); // 第一个按钮y=50，每个间隔47像素（42高度+5间距）
+                    buttonRect.anchoredPosition = pos;
+                }
             }
         }
 
